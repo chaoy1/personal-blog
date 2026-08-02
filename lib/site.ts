96e11ed361d3728ffdda1a94f655dc11868a1b35
ChoyChou <zhouchaoyi03@foxmail.com>
2026-08-02 10:23:32 +0800

    更名似水流年，更新诗句与图标

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,3 @@
-export const SITE_NAME = '墨痕'
-export const SITE_DESC = '一个人的纸上纪事'
+export const SITE_NAME = '似水流年'
+export const SITE_DESC = '流光容易把人抛，红了樱桃，绿了芭蕉'
 export const SITE_VERSE = '记录那些值得被记住的片刻'
